d761e4195883fc70794f926d51480a57ea5a61a8
McIvor Steiner <mcivorsteiner@gmail.com>
2014-04-24 15:43:04 -0700

    Added following method to user model. Allows users to see who they are following.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
   has_many :tweets
   has_many :followings
   has_many :followers, class_name: "User", through: :followings, foreign_key: "follower_id"
+
+  def following
+    following = Following.where("follower_id = ?", self.id)
+    following.map! { |followee| User.find(followee.user_id) }
+    following
+  end
 end
