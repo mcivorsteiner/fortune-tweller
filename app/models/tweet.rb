2a688141e1c3a7a8f55e72b405eaf7b3e37c7c08
Apprentice <apprentice@dbc04.local>
2014-04-25 12:35:02 -0700

    user cannot tweet a blank message (and gets an error)

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,8 @@
 class Tweet < ActiveRecord::Base
+  validates :message, :presence => true, :length => {
+    :in => 1..140,
+    :too_short => "Tweet must be at least one character.",
+    :too_long => "Tweet must be less than 140 characters."}
   belongs_to :user
   has_many :retweets
-
-  validates :message, length: {maximum: 140}
 end
