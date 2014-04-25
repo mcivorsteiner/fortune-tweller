82cedae1c2ffd09c14c143c238f428c2a9017c8d
McIvor Steiner <mcivorsteiner@gmail.com>
2014-04-24 17:05:54 -0700

    Added link to followers and following on profile controller.

--- app/controllers/profile.rb
+++ app/controllers/profile.rb
@@ -2,13 +2,20 @@ get '/profile/:handle' do
   if logged_in?
     @user = current_user
     @tweets = @user.tweets
-  erb :profile
+    erb :profile
+  else
+    redirect '/'
+  end
 end
 
 get '/profile/:handle/followers' do
-
+  @user = current_user
+  @followers = @user.followers
+  erb :followers
 end
 
 get '/profile/:handle/following' do
-
+  @user = current_user
+  @following = @user.following
+  erb :following
 end
