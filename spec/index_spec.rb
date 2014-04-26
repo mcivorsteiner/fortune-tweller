9de54125fc51643b50a1f9bcd3eb0df1d134867f
McIvor Steiner <mcivorsteiner@gmail.com>
2014-04-26 00:31:07 -0700

    added user creation test and 4 follower tests

--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -2,35 +2,19 @@ require 'spec_helper'
 
 # For testing your controller. Use RSpec and Rack/Test for testing your routes in index.rb
 
+# describe "get / route" do
+#   it "shows a sign-up and login screen" do
+#     ## Arrange
 
-describe "Index (this is a skeleton index.rb test!)" do
+#     ## Act
+#     get '/'
 
-  describe 'user sign-up' do
-    it 'should return a user' do
-      lou = User.create(name: 'lou', email: 'lou@smail.com', handle: 'loulou', password_hash: 'LL')
-        expect (lou.class.name).to eq "lou"
-    end
-  end
-end
-
-
-# ----------------------------------
-  # describe '/profile' do
-  #     it { should route(:get, "/profile/:handle") }
-  # end
-
-  # describe "index" do
-  #   it "renders the home page" do
-  #     get :index
-  #       expect(last_response.location).to include("Fortune cookie + twitter.")
-  #   end
-  # end
+#     ## Assert
+#     expect(last_response.body).to include('signup', 'login')
+#   end
+# end
 
 
-  # describe "handling AccessDenied exceptions" do
-  #   it "redirects to the /invalid page" do
-  #     get :invalid
-  #     response.should redirect_to("/invalid")
-  #   end
-  # end
+
+
 
